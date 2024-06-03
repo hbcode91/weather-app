97eeff09ebfea0c6a5d41e0c6191f1d5dea29e0e
Hilmi BOUALLAGUE <hilmi.bouallague@se.com>
2024-06-03 09:57:49 +0200

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageModule } from "./components/main-page/main-page.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MainPageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
